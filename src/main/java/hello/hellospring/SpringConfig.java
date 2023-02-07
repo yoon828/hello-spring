7c79c7b19168de92e9822aa02457f86a5a9bfce9
yoon <yoon828990@gmail.com>
2023-02-07 18:53:55 +0900

    스프링 빈과 의존관계

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,24 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+//직접 빈을 등록하는 방법
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+}
